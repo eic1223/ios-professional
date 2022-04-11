99f032fd48b9d733d06d4e585f3a1acbdc287b5b
dane_won <eic1223@naver.com>
2022-04-11 09:56:56 +0900

    feat: handle network errors

--- Banky/Banky/AccountSummaryViewController.swift
+++ Banky/Banky/AccountSummaryViewController.swift
@@ -140,6 +140,7 @@ extension AccountSummaryViewController {
 //                self.tableView.reloadData()
             case .failure(let error):
                 print(error.localizedDescription)
+                
             }
             group.leave()
         }
@@ -153,6 +154,7 @@ extension AccountSummaryViewController {
 //                self.tableView.reloadData()
             case .failure(let error):
                 print(error.localizedDescription)
+                self.displayError(error)
             }
             group.leave()
         }
@@ -164,7 +166,7 @@ extension AccountSummaryViewController {
             
             self.isLoaded = true
             self.configureTableHeaderView(with: profile)
-            self.configureTableHeaderView(with: self.accounts)
+            self.configureTableCells(with: self.accounts)
             self.tableView.reloadData()
         }
     }
@@ -178,6 +180,28 @@ extension AccountSummaryViewController {
         accountCellViewModel = accounts.map {
             AccountSummaryCell.ViewModel(accountType: $0.type, accountName: $0.name, balance: $0.amount)
         }
+    }
+    
+    private func displayError(_ error: NetworkError) {
+        let title: String
+        let message: String
+        switch error {
+        case .serverError:
+            title = "Server Error"
+            message = "Ensure you are connected to the internet. Please try again."
+        case .decodingError:
+            title = "Decoding Error"
+            message = "We could not process your request. Please try again."
+        }
+        self.showErrorAlert(title: title, message: message)
+    }
+    
+    private func showErrorAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        
+        present(alert, animated: true, completion: nil)
     }
 }
 
